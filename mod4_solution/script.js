71ee229faab5b6364c7d7169c07486e29f624823
Ferxoz <66985760+Ferxoz@users.noreply.github.com>
2020-08-31 19:35:29 -0500

    Update script.js

--- mod4_solution/script.js
+++ mod4_solution/script.js
@@ -1,7 +1,7 @@
 (function (window) {
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-  for (var i = 0; i < names.length; i++) {
+  for (var i in names) {
     var firstLetter = names[i].charAt(0).toLowerCase();
   if (firstLetter == 'j') {
     window.byeSpeaker.speak(names[i]);
